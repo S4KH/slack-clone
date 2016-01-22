35796b1a86faf45679279cf468efa1512d124865
Sam Kh <sam.kh@dannys-Mac-mini.local>
2016-01-22 13:23:55 +1100

    .gitignore added, channels sub page added, home,login,register resolve changed to channels

---
+++ app/channels/channels.controller.js
@@ -0,0 +1,12 @@
+angular.module('fireSlack')
+.controller('ChannelsCtrl', function($state, Auth, Users, profile, channels){
+	var channelsCtrl = this;
+	channelsCtrl.profile = profile;
+	channelsCtrl.channels = channels;
+	channelsCtrl.getDisplayName = Users.getDisplayName;
+	channelsCtrl.getGravatar = Users.getGravatar;
+	channelsCtrl.logout = function(){
+		Auth.$unauth();
+		$state.go('home');
+	};
+});
---
+++ app/channels/channels.service.js
@@ -0,0 +1,7 @@
+angular.module('fireSlack')
+.factory('Channels', function($firebaseArray, FirebaseUrl){
+	var ref = new Firebase(FirebaseUrl+'channels');
+	var channels = $firebaseArray(ref);
+
+	return channels;
+});
